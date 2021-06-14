3b70ad42bac6c5f4c1b7c642e112b1266f0712de
Max Base <MaxBaseCode@gmail.com>
2021-06-14 14:58:28 +0430

    Design test-debug form in browser

--- graphql.php
+++ graphql.php
@@ -1,7 +1,6 @@
 <?php
 declare(strict_types=1);
 
-header('Content-Type: application/json; charset=UTF-8');
 require_once __DIR__ . '/vendor/autoload.php';
 use GraphQL\GraphQL;
 use GraphQL\Utils\BuildSchema;
@@ -18,10 +17,31 @@ try {
     'prefix' => 'You said: ',
   ];
 
-  $rawInput       = file_get_contents('php://input');
-  $input          = json_decode($rawInput, true);
-  $query          = $input['query'];
-  $variableValues = $input['variables'] ?? null;
+  if(isset($_POST["submit"])) {
+    $query          = $_POST['query'] ?? null;
+    $variableValues = $_POST['variables'] ?? null;
+    if($variableValues === "") $variableValues = null;
+  } else {
+    $rawInput       = file_get_contents('php://input');
+    $input          = json_decode($rawInput, true);
+    $query          = $input['query'] ?? null;
+    $variableValues = $input['variables'] ?? null;
+  
+    if($input === null) {
+      print '<form action="" method="POST">';
+      print '<b>Query:</b><br>';
+      print '<textarea name="query"></textarea>';
+      print '<br>';
+      print '<b>Variables:</b><br>';
+      print '<textarea name="variables"></textarea>';
+      print '<br>';
+      print '<button name="submit">Submit</button>';
+      print '</form>';
+      exit();
+    }
+  }
+
+  header('Content-Type: application/json; charset=UTF-8');
 
   $result = GraphQL::executeQuery($schema, $query, $rootValue, null, $variableValues);
 } catch (Throwable $e) {
