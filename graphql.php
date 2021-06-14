67fc70d2b2254634e1aacc57ddc570817f2886a2
Max Base <MaxBaseCode@gmail.com>
2021-06-14 15:02:23 +0430

    Fix bug of dbl qut at html form printing

--- graphql.php
+++ graphql.php
@@ -5,16 +5,16 @@ require_once __DIR__ . "/vendor/autoload.php";
 use GraphQL\GraphQL;
 use GraphQL\Utils\BuildSchema;
 
-try {
-  $resolveEcho = require "echo.php";
-  $resolveSum = require "sum.php";
+$resolveEcho = require "echo.php";
+$resolveSum = require "sum.php";
 
+try {
   $schema = file_get_contents("schema.graphql");
   $schema = BuildSchema::build($schema);
   $rootValue = [
     "echo" => $resolveEcho,
     "sum" => $resolveSum,
-    "prefix" => "",
+    "prefix" => "Welcome, Dear ",
   ];
 
   if(isset($_POST["submit"])) {
@@ -28,14 +28,14 @@ try {
     $variableValues = $input["variables"] ?? null;
   
     if($input === null) {
-      print "<form action="" method="POST">";
+      print "<form action=\"\" method=\"POST\">";
       print "<b>Query:</b><br>";
-      print "<textarea name="query"></textarea>";
+      print "<textarea name=\"query\"></textarea>";
       print "<br>";
       print "<b>Variables:</b><br>";
-      print "<textarea name="variables"></textarea>";
+      print "<textarea name=\"variables\"></textarea>";
       print "<br>";
-      print "<button name="submit">Submit</button>";
+      print "<button name=\"submit\">Submit</button>";
       print "</form>";
       exit();
     }
